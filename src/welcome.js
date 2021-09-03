b8a1566835f9d517b8235466ff54c44d6328ce5f
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2021-09-03 16:21:30 +0530

    Adding CSS abstraction and Comments in CSS file

--- src/welcome.js
+++ src/welcome.js
@@ -22,93 +22,85 @@ class WelcomePage extends Component{
       <body className="app-bg-color">
         <header className="h-auto">
           <div
-            className="w-screen lg:pb-24 pb-5 bg-cover"
+            className="header-img"
             style={{ backgroundImage: `url(${WelcomePageImage})` }}
           >
-            <div className="w-full lg:p-0 px-4 py-2">
-              <div className="lg:h-12 w-full flex items-center justify-center">
-                <div className="lg:h-12 w-full max-w-7xl flex justify-end items-center pt-2">
-                  <div className="nav-welcome-btn-no-bg">
+            <div className="header-div">
+              <div className="navbar-bg">
+                <div className="navbar-fg">
+                  <div className="navbar-btn-no-bg">
                     <button
-                      className="text-white lg:text-xl text-center text-sm w-full"
+                      className="navbar-btn-no-bg-text"
                       id="feature"
                       onClick={this.handleFeatureOnclick}
                     >
                       Features
                     </button>
                   </div>
-                  <div className="nav-welcome-btn-no-bg">
-                    <button className="text-white lg:text-xl text-sm w-32  ">
-                      Data
-                    </button>
+                  <div className="navbar-btn-no-bg">
+                    <button className="navbar-btn-no-bg-text">Data</button>
                   </div>
-                  <button className="nav-login-btn-w-bg">
-                    <div className="text-white lg:text-lg text-xs text-center lg:w-32 w-20 ">
-                      LOGIN
-                    </div>
+                  <button className="navbar-btn-w-bg">
+                    <div className="navbar-btn-w-bg-text">LOGIN</div>
                   </button>
                 </div>
               </div>
-              <div className="flex justify-center items-center">
-                <div className="h-auto w-full max-w-7xl flex flex-col justify-between items-center lg:px-10 lg:py-4">
-                  <div className=" lg:text-8xl font-serif text-3xl tracking-widest text-white mt-10 lg:mt-40">
-                    PENTAGON
-                  </div>
-                  <div className="lg:text-3xl text-sm text-white lg:mt-12 mt-2 text-center max-w-6xl">
+              <div className="wel-sec">
+                <div className="wel-sec-div">
+                  <div className="app-title">PENTAGON</div>
+                  <div className="app-description">
                     <p>
                       The platform to help you manage all your projects at one
                       place.
                     </p>
                   </div>
-                  <div className="flex justify-center items-center lg:mt-64 mt-20 w-full">
-                    <button className="lg:w-52 w-28 text-white lg:border-3 border-2 py-1 lg:py-2 lg:text-lg text-xs lg:mr-40 mr-10 border-white rounded-full ">
+                  <div className="header-util-sec">
+                    <button className="bfr-you-strt-btn">
                       Before you Start
                     </button>
-                    <button className="lg:w-52 w-28 text-white btn-bg-color lg:py-3 py-2 rounded-full lg:text-lg text-xs">
-                      Start Here
-                    </button>
+                    <button className="strt-here-btn">Start Here</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </header>
-        <div className="w-full flex justify-center items-center">
+        <div className="details-section">
           <div className="feature-grid">
             <div className="sub-pg-title">Features</div>
-            <div className="lg:flex max-w-7xl lg:justify-start lg:items-center">
-              <div className="lg:w-1/3 flex flex-col justify-start items-center p-5">
+            <div className="features-listing-section">
+              <div className="feature-card">
                 <img
                   alt="Collaboration"
                   src={Collaboration}
-                  className="lg:w-1/4 w-1/6 mb-6"
+                  className="feature-sq-icons"
                 ></img>
                 <font className="sub-card-title">Collaborate</font>
-                <p className="w-full font-bg-color lg:text-lg text-sm text-center px-8">
+                <p className="feature-description-text">
                   Collaborate with teammates to manage and complete projects in
                   a single environment.
                 </p>
               </div>
-              <div className="lg:w-1/3 flex flex-col justify-start items-center p-5">
+              <div className="feature-card">
                 <img
                   alt="Collaboration"
                   src={Database}
-                  className=" lg:w-1/4 w-1/6 mb-7"
+                  className=" feature-db-icon"
                 ></img>
                 <font className="sub-card-title">File Storage</font>
-                <p className="w-full font-bg-color lg:text-lg text-sm text-center px-8">
+                <p className="feature-description-text">
                   Easy to store and share files related to the project and also
                   mention files specific to a task.
                 </p>
               </div>
-              <div className="lg:w-1/3 flex flex-col justify-start items-center p-5">
+              <div className="feature-card">
                 <img
                   alt="Collaboration"
                   src={trackProgress}
-                  className="lg:w-1/4 w-1/6 mb-6"
+                  className="feature-sq-icons"
                 ></img>
                 <font className="sub-card-title">Track Progress</font>
-                <p className="w-full font-bg-color lg:text-lg text-sm text-center px-8">
+                <p className="feature-description-text">
                   Track your and the team’s progress status in one go.
                 </p>
               </div>
@@ -146,9 +138,13 @@ class WelcomePage extends Component{
             </div>
             <div className="data-bg-grid">
               <div className="sub-pg-title">How safe is your data?</div>
-              <div className="flex justify-start items-start lg:px-0 px-8">
-                <img alt="Secure Database" src={secureData} className="lg:w-auto w-16"></img>
-                <div className="lg:text-2xl text-sm pl-8">
+              <div className="data-sub-grid">
+                <img
+                  alt="Secure Database"
+                  src={secureData}
+                  className=""
+                ></img>
+                <div className="data-description-text">
                   <p>
                     All your project details and files are safe in our database.
                     Not even we can access it due to AES 256 Encryption, you can
@@ -160,7 +156,7 @@ class WelcomePage extends Component{
           </div>
         </div>
         <footer>
-          <div className="w-screen app-bg-color border-2 mt-10 border-red-400"></div>
+          <div className="footer-line"></div>
           <div className="">Contact the Developers</div>
         </footer>
       </body>
