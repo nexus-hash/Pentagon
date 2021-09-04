609987415f99550867d252d0333018450e13420f
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2021-09-05 00:41:47 +0530

    New:Added Signup Form Checks needFix:Fix reset of form data upon signup click while processing

--- src/pages/Authentication/signupPage/signup.js
+++ src/pages/Authentication/signupPage/signup.js
@@ -10,16 +10,27 @@ class signup extends Component{
 
   constructor(props){
     super(props);
-    this.state={
-      loginMessage:"",
-      usernameMessage:"",
+    this.state = {
+      loginMessage: "",
+      usernameMessage: "",
       passwordMatch: "",
-      name:"",
-      username:"",
-      email:"",
-      password:"",
-      confirmPassword:""
-    }
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      oneNum: false,
+      oneCap: false,
+      oneSpecial: false,
+      lengthEight: false,
+      emailCheck:false,
+      oneNumColor: "#DC2626",
+      oneCapColor: "#DC2626",
+      oneSpecialColor: "#DC2626",
+      lengthEightColor: "#DC2626",
+      btnBg:0.6,
+      buttonState: true,
+    };
     this.handleVisibilityClick = this.handleVisibilityClick.bind(this);
     this.handleUsernameChange  = this.handleUsernameChange.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
@@ -27,6 +38,38 @@ class signup extends Component{
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
     this.handleUserRegistrationOnClick = this.handleUserRegistrationOnClick.bind(this);
+    this.checkSubmissionForm = this.checkSubmissionForm.bind(this);
+  }
+
+  checkSubmissionForm (){
+    setTimeout(() => {
+      if (
+        this.state.oneCap &&
+        this.state.oneNum &&
+        this.state.oneSpecial &&
+        this.state.lengthEight &&
+        this.state.passwordMatch === "" &&
+        this.state.usernameMessage === "" &&
+        this.state.username !=="" &&
+        this.state.email !==""&&
+        this.state.name !=="" &&
+        this.state.emailCheck
+      ) {
+        this.setState({
+          btnBg: 1,
+        });
+        this.setState({
+          buttonState:false,
+        })
+      } else {
+        this.setState({
+          btnBg: 0.6,
+        });
+        this.setState({
+          buttonState: true,
+        });
+      }
+    }, 100);
   }
 
   handleVisibilityClick(){
@@ -49,19 +92,106 @@ class signup extends Component{
     this.setState({
       name: event.target.value
     })
+    this.checkSubmissionForm();
   }
 
   handleUsernameChange (event){
     this.setState({
       username: event.target.value,
     });
+    this.checkSubmissionForm();
   }
 
   handlePasswordChange(event){
     var submitButton = document.getElementById('submitButton');
+    var capitalAlphabet =new RegExp( "^(?=.*[A-Z])");
+    var oneNumeric = new RegExp("^(?=.*\\d)");
+    var specialChar = new RegExp( "^(?=.*[-+_!@#$%^&*., ?])+");
+
+
     this.setState({
       password: event.target.value,
     });
+
+    if(event.target.value.length>7){
+      this.setState({
+        lengthEight:true
+      })
+      if(this.state.lengthEightColor === "#DC2626"){
+        this.setState({
+          lengthEightColor: "#059669",
+        });
+      }
+    }else{
+      this.setState({
+        lengthEight: false,
+      });
+      if (this.state.lengthEightColor === "#059669") {
+        this.setState({
+          lengthEightColor: "#DC2626",
+        });
+      }
+    }
+
+    if(capitalAlphabet.test(event.target.value)){
+      this.setState({
+        oneCap:true
+      })
+      if (this.state.oneCapColor === "#DC2626") {
+        this.setState({
+          oneCapColor: "#059669",
+        });
+      }
+    }else{
+      this.setState({
+        oneCap: false,
+      });
+      if (this.state.oneCapColor === "#059669") {
+        this.setState({
+          oneCapColor: "#DC2626",
+        });
+      }
+    }
+
+    if (oneNumeric.test(event.target.value)) {
+      this.setState({
+        oneNum:true
+      })
+      if (this.state.oneNumColor === "#DC2626") {
+        this.setState({
+          oneNumColor: "#059669",
+        });
+      }
+    }else{
+      this.setState({
+        oneNum: false,
+      });
+      if (this.state.oneNumColor === "#059669") {
+        this.setState({
+          oneNumColor: "#DC2626",
+        });
+      }
+    }
+
+    if (specialChar.test(event.target.value)) {
+      this.setState({
+        oneSpecial:true,
+      })
+      if (this.state.oneSpecialColor === "#DC2626") {
+        this.setState({
+          oneSpecialColor: "#059669",
+        });
+      }
+    }else{
+        this.setState({
+          oneSpecial: false,
+        });
+        if (this.state.oneSpecialColor === "#059669") {
+          this.setState({
+            oneSpecialColor: "#DC2626",
+          });
+        }
+    }
 
     if (this.state.confirmPassword === event.target.value) {
       this.setState({
@@ -73,6 +203,8 @@ class signup extends Component{
         passwordMatch: "Confirm Password and Password does not match.",
       });
     }
+    this.checkSubmissionForm();
+    
   }
 
   handleConfirmPasswordChange(event){
@@ -89,14 +221,26 @@ class signup extends Component{
       this.setState({
         passwordMatch:"Confirm Password and Password does not match.",
       });
-      
     }
+    this.checkSubmissionForm();
   }
 
   handleEmailChange (event){
     this.setState({
       email: event.target.value,
     });
+    const re =
+      /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(event.target.value)){
+      this.setState({
+        emailCheck:true,
+      })
+    }else{
+      this.setState({
+        emailCheck: false,
+      });
+    }
+      this.checkSubmissionForm();
   }
 
   handleUserRegistrationOnClick(){
@@ -112,136 +256,141 @@ class signup extends Component{
           </div>
         </div>
         <form className="xl:w-1/3 lg:w-1/2 sm:w-3/4 max-w-7xl w-full lg:p-0 px-6 h-auto flex flex-col justify-between space-y-2 items-center">
-            <div className="lg:text-3xl text-2xl sm:font-mono font-serif font-bg-color font-semibold tracking-wide lg:mb-2">
-              Introduce Yourself
-            </div>
-            <div className="text-red-700 font-medium lg:text-lg text-sm">
-              {this.loginMessage}
-            </div>
+          <div className="lg:text-3xl text-2xl sm:font-mono font-serif font-bg-color font-semibold tracking-wide lg:mb-2">
+            Introduce Yourself
+          </div>
+          <div className="text-red-700 font-medium lg:text-lg text-sm">
+            {this.loginMessage}
+          </div>
 
+          <input
+            type="text"
+            placeholder="Username"
+            className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 focus:shadow-xl focus:outline-none focus:border-transparent"
+            onChange={this.handleUsernameChange}
+            value={this.state.username}
+            required
+          ></input>
+          <font className="text-green-600 w-full lg:text-sm text-xs text-left">
+            {this.usernameMessage}
+          </font>
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 mb-1 focus:shadow-xl focus:outline-none focus:border-transparent"
+            onChange={this.handleNameChange}
+            value={this.state.name}
+            required
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 mb-1 focus:shadow-xl focus:outline-none focus:border-transparent"
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+            required
+          ></input>
+          <div className="w-full bg-white flex justify-center pr-2 items-center border-2 rounded-lg border-gray-100 mb-1 ">
             <input
-              type="text"
-              placeholder="Username"
-              className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 focus:shadow-xl focus:outline-none focus:border-transparent"
-              onChange={this.handleUsernameChange}
-              value={this.state.username}
-              required
-            ></input>
-            <font className="text-green-600 w-full lg:text-sm text-xs text-left">
-              {this.usernameMessage}
-            </font>
-            <input
-              type="text"
-              placeholder="Name"
-              className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 mb-1 focus:shadow-xl focus:outline-none focus:border-transparent"
-              onChange={this.handleNameChange}
-              value={this.state.name}
-              required
-            ></input>
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 mb-1 focus:shadow-xl focus:outline-none focus:border-transparent"
-              onChange={this.handleEmailChange}
-              value={this.state.email}
-              required
-            ></input>
-            <div className="w-full bg-white flex justify-center pr-2 items-center border-2 rounded-lg border-gray-100 mb-1 ">
-              <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                className="px-4 py-2 w-full focus:shadow-xl focus:outline-none focus:border-transparent rounded-lg"
-                onChange={this.handlePasswordChange}
-                value={this.state.password}
-                required
-              ></input>
-              <div id="visible" className="">
-                <VisibilityIcon
-                  className="cursor-pointer hover:"
-                  id="eyeIcon"
-                  onClick={this.handleVisibilityClick}
-                ></VisibilityIcon>
-              </div>
-              <div id="notVisible" className="hidden">
-                <VisibilityOffIcon
-                  className="cursor-pointer hover:shadow-md"
-                  id="eyeIcon"
-                  onClick={this.handleVisibilityClick}
-                ></VisibilityOffIcon>
-              </div>
-            </div>
-            <div className="w-full sm:spcae-y-0 space-y-1">
-              <div className="w-full px-2 sm:flex justify-start items-center sm:tracking-tight sm:space-x-1 space-y-1 lg:space-y-0 text-sm">
-                <div className="text-green-600 sm:w-1/2 w-full flex items-center">
-                  <CheckCircleIcon
-                    id="schar"
-                    fontSize="small"
-                    className="text-red-600 mr-1"
-                  ></CheckCircleIcon>
-                  {"   "}
-                  At least one special character
-                </div>
-                <div className="text-green-600 sm:w-1/2 flex items-center">
-                  <CheckCircleIcon
-                    id="calphabet"
-                    fontSize="small"
-                    className="text-red-600 mr-1"
-                  ></CheckCircleIcon>
-                  {"   "}
-                  Minimum one capital alphabet
-                </div>
-              </div>
-              <div className="w-full px-2 sm:flex justify-start items-center sm:tracking-tight sm:space-x-1 space-y-1 lg:space-y-0 text-sm">
-                <div className="text-green-600 w-1/2 flex items-center">
-                  <CheckCircleIcon
-                    id="num"
-                    fontSize="small"
-                    className="text-red-600 mr-1"
-                  ></CheckCircleIcon>
-                  {"   "}
-                  At least one number
-                </div>
-                <div className="text-green-600 w-1/2 flex items-center">
-                  <CheckCircleIcon
-                    id="minchar"
-                    fontSize="small"
-                    className="text-red-600 mr-1"
-                  ></CheckCircleIcon>
-                  {"   "}
-                  Minimum 8 characters
-                </div>
-              </div>
-            </div>
-            <div className="w-full px-2 flex justify-start items-center space-x-4 text-xs"></div>
-            <input
+              id="password"
               type="password"
-              placeholder="Confirm Password"
-              className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 focus:shadow-xl focus:outline-none focus:border-transparent"
-              onChange={this.handleConfirmPasswordChange}
-              value={this.state.confirmPassword}
+              placeholder="Password"
+              className="px-4 py-2 w-full focus:shadow-xl focus:outline-none focus:border-transparent rounded-lg"
+              onChange={this.handlePasswordChange}
+              value={this.state.password}
               required
             ></input>
-            <div className="text-red-600 w-full px-2 lg:text-xs text-sm text-left">
-              {this.state.passwordMatch}
+            <div id="visible" className="">
+              <VisibilityIcon
+                className="cursor-pointer hover:"
+                id="eyeIcon"
+                onClick={this.handleVisibilityClick}
+              ></VisibilityIcon>
             </div>
-            <div></div>
-            <button
-              onClick={this.handleUserRegistrationOnClick}
-              id="submitButton"
-              type="submit"
-              className="w-full py-2 rounded-lg text-center btn-bg-color-not-allowed text-white hover:shadow-lg cursor-not-allowed"
+            <div id="notVisible" className="hidden">
+              <VisibilityOffIcon
+                className="cursor-pointer hover:shadow-md"
+                id="eyeIcon"
+                onClick={this.handleVisibilityClick}
+              ></VisibilityOffIcon>
+            </div>
+          </div>
+          <div className="w-full sm:spcae-y-0 space-y-1">
+            <div className="w-full px-2 sm:flex justify-start items-center sm:tracking-tight sm:space-x-1 space-y-1 lg:space-y-0 text-sm">
+              <div className="text-green-600 sm:w-1/2 w-full flex items-center">
+                <CheckCircleIcon
+                  id="schar"
+                  fontSize="small"
+                  className="mr-1"
+                  style={{ color: this.state.oneSpecialColor }}
+                ></CheckCircleIcon>
+                {"   "}
+                At least one special character
+              </div>
+              <div className="text-green-600 sm:w-1/2 flex items-center">
+                <CheckCircleIcon
+                  id="calphabet"
+                  fontSize="small"
+                  className=" mr-1"
+                  style={{ color: this.state.oneCapColor }}
+                ></CheckCircleIcon>
+                {"   "}
+                Minimum one capital alphabet
+              </div>
+            </div>
+            <div className="w-full px-2 sm:flex justify-start items-center sm:tracking-tight sm:space-x-1 space-y-1 lg:space-y-0 text-sm">
+              <div className="text-green-600 w-1/2 flex items-center">
+                <CheckCircleIcon
+                  id="num"
+                  fontSize="small"
+                  className="mr-1"
+                  style={{ color: this.state.oneNumColor }}
+                ></CheckCircleIcon>
+                {"   "}
+                At least one number
+              </div>
+              <div className="text-green-600 w-1/2 flex items-center">
+                <CheckCircleIcon
+                  id="minchar"
+                  fontSize="small"
+                  className=" mr-1"
+                  style={{ color: this.state.lengthEightColor }}
+                ></CheckCircleIcon>
+                {"   "}
+                Minimum 8 characters
+              </div>
+            </div>
+          </div>
+          <div className="w-full px-2 flex justify-start items-center space-x-4 text-xs"></div>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="w-full bg-white px-4 py-2 rounded-lg border-2 border-gray-100 focus:shadow-xl focus:outline-none focus:border-transparent"
+            onChange={this.handleConfirmPasswordChange}
+            value={this.state.confirmPassword}
+            required
+          ></input>
+          <div className="text-red-600 w-full px-2 lg:text-xs text-sm text-left">
+            {this.state.passwordMatch}
+          </div>
+          <div></div>
+          <button
+            onClick={this.handleUserRegistrationOnClick}
+            id="submitButton"
+            className="w-full py-2 rounded-lg text-center btn-bg-color text-white hover:shadow-lg cursor-pointer"
+            style = {{opacity:this.state.btnBg}}
+            disabled={this.state.buttonState}
+          >
+            Sign Up
+          </button>
+          <div className="w-full py-2 flex justify-between items-center">
+            <Link
+              to="/login"
+              className="w-full py-2 border-2 text-center border-gray-400 font-bg-color rounded-lg text-sm"
             >
-              Sign Up
-            </button>
-            <div className="w-full py-2 flex justify-between items-center">
-              <Link
-                to="/login"
-                className="w-full py-2 border-2 text-center border-gray-400 font-bg-color rounded-lg text-sm"
-              >
-                Login?
-              </Link>
-            </div>
+              Login?
+            </Link>
+          </div>
         </form>
         <div className="h-20 w-full "></div>
       </div>
